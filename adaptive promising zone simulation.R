6bf3383e119d77d189b8e9247eb57c32cb107772
william7129 <zyh19940205@gmail.com>
2022-03-31 11:02:40 +0800

    final

--- adaptive promising zone simulation.R
+++ adaptive promising zone simulation.R
@@ -1,8 +1,10 @@
+# simulating trials with binomial endpoint to evaluate operating characteristics
+# of an adaptive design based on promising zone with futility and efficacy stop
+
 if (!require("gsDesign")) {
   install.packages("gsDesign")
   library("gsDesign")
 }
-
 
 # parameter description ---------------------------------------------------
 
@@ -18,10 +20,11 @@ if (!require("gsDesign")) {
 #' @param sfu alpha spending function , check package `gsDesign`
 #' @param sfupar alpha spending function parameter, check package `gsDesign`
 #' @param cp_fu   futility boundary based on conditional power
+#' @param adjust   Miettinen and Nurminen test statistic This includes a factor of n / (n - 1) where n is the total sample size. If adj is not 1, this factor is not applied. The default is adj=0 since nominal Type I error is generally conservative with adj=1 
 #' @param scale estimator: "difference", "or", "rr" 
 #' @param overrun overrun during interim analysis
 #' @return operating characteristics of adaptive design
-
+#' @note  sample size calculation is based on fixed design
 
 pz.sim.binary <- function(p1,
                           p2,
@@ -32,6 +35,7 @@ pz.sim.binary <- function(p1,
                           pz_high,
                           nsim,
                           cp_fu,
+                          adjust = 0,
                           sfu = NULL,
                           sfupar = NULL,
                           k = 2,
@@ -39,22 +43,16 @@ pz.sim.binary <- function(p1,
                           seed = 1,
                           overrun = 15,
                           n1 = NULL,
-                          n2 = NULL,
-                          futility_b = NULL
+                          n2 = NULL
 ){
   set.seed(seed)
   scale = match.arg(tolower(scale), c("difference", "rr", "or"))
-  
-  # size = nBinomial(p1=p1,p2=p2,
-  #                  alpha=alpha,beta = beta, sided=1,
-  #                  scale = scale,
-  #                  n=NULL)
-  
+  # efficacy stop boundary
   design = gsDesign(k = k,test.type = 1,  alpha = alpha,
                     n.fix= 100, # sample size does not affect boundary calculation
                     timing = c(timing),
                     sfu = sfu,
-                    sfupar = 1,
+                    sfupar = sfupar,
                     endpoint = 'Binomial')
   
   
@@ -63,57 +61,62 @@ pz.sim.binary <- function(p1,
   #stage 2 boundary
   z2_bound = design$upper$bound[2]
   
-  #stage 1 sample size
-  # if(is.null(n1)){n1 = ceiling(design$n.I)[1]}
-  #planned sample size
-  # if(is.null(n2)){n2 = ceiling(design$n.I)[2]}
-  #maintain equal allocation
   is.even <- function(x){ x %% 2 == 0 }
-  if(is.even(n1) == F){n1 = n1 + 1; n2 = n2 + 1}
-  if(is.even(n2) == F){n2 = n2 + 1}
+  if(!is.even(n1)){n1 = n1 + 1; n2 = n2 + 1}
+  if(!is.even(n2)){n2 = n2 + 1}
   #stage 2 sample size
   n2_tilde = n2 - n1
   #simulate trials
   results = as.data.frame(matrix(NA, nrow=nsim, ncol=18)) 
-  colnames(results) <- c('Stage 1 sample size', 'Stage 2 sample size (planned)',
-                         'Total sample size (planned)',
-                         'Stage 2 sample size (re-estimated)','Total sample size (re-estimated)',
-                         'Stage 1 TRT success','Stage 1 TRT failure',
-                         'Stage 1 CTRL success','Stage 1 CTRL failure',
-                         'Chisq','Wald Z  w./ Cont','CP','Relative risk reduction','Risk diff',
-                         'Wald_adaptive', "Success_adaptive",'Wald_sequential',"Success_sequential")
-  results[,1] = n1
-  results[,2] = n2_tilde
-  results[,3] = n2
+  colnames(results) <- c('Stage 1 sample size', # 1
+                         'Stage 2 sample size (planned)',# 2
+                         'Total sample size (planned)',# 3
+                         'Stage 2 sample size (re-estimated)',# 4
+                         'Total sample size (re-estimated)',# 5
+                         'Stage 1 TRT success',# 6
+                         'Stage 1 TRT failure',# 7
+                         'Stage 1 CTRL success',# 8
+                         'Stage 1 CTRL failure',# 9
+                         'Chisq',# 10
+                         'Wald Z  w./ Cont',# 11
+                         'CP',# 12
+                         'Relative risk reduction',# 13
+                         'Risk diff',# 14
+                         'Wald_adaptive', # 15
+                         "Success_adaptive",# 16
+                         'Wald_sequential',# 17
+                         "Success_sequential")# 18
+  results[ ,1] = n1
+  results[ ,2] = n2_tilde
+  results[ ,3] = n2
   #data
-  results[,6] = rbinom(nsim, n1/2,p1)#response in treatment group
-  results[,7] = n1/2 - results[,6]
-  results[,8] = rbinom(nsim, n1/2,p2)#response in control group
-  results[,9] = n1/2 - results[,8]
-  # results[,13] = (results[,6]/(n1/2) - results[,8]/(n1/2)) / (results[,8]/(n1/2))
-  results[,14] = (results[,6]/(n1/2)) - (results[,8]/(n1/2))
+  results[ ,6] = rbinom(nsim, n1/2, p1)# response in treatment group
+  results[ ,7] = n1/2 - results[ ,6]
+  results[ ,8] = rbinom(nsim, n1/2, p2)# response in control group
+  results[ ,9] = n1/2 - results[ ,8]
+  results[ ,14] = (results[ ,6]/(n1/2)) - (results[ ,8]/(n1/2))
   #test statistic in stage 1: z1
-  results[,11] = apply(results[,6:9],1,function(x) 
-    testBinomial(x[1],x[3],x[1]+x[2],x[3]+x[4],adj=1,scale = scale))
+  results[,11] = apply(results[,6:9], 1, function(x) 
+    testBinomial(x[1], x[3], x[1]+x[2], x[3]+x[4], adj = adjust, scale = scale))
   #CP
-  cp <- function(n1,n2,z1){
+  cp <- function(n1, n2, z1){
     n2_tilde = n2-n1
     1 - pnorm(((z2_bound*sqrt(n2) - z1*sqrt(n1)) / sqrt(n2_tilde)) -
                 ((z1*sqrt(n2_tilde)) / sqrt(n1)))
   }
-  results[,12] = apply(results[,1:11],1,function(x)
+  results[,12] = apply(results[ ,1:11], 1, function(x)
     cp(x[1], x[3], x[11]))
   
   
   #re-estimate  sample size
-  re.est <- function(pz_low,pz_high,n_max,n1,n2,z1,cp){
+  re.est <- function(pz_low, pz_high, n_max, n1, n2, z1, cp){
     if(cp >= pz_low & cp < pz_high & z1 < z1_bound){
       n2_tilde_prime = (n1/(z1^2))*((z2_bound*sqrt(n2)-z1*sqrt(n1))/
                                       sqrt(n2-n1) + qnorm(pz_high))^2
       n2_prime = n2_tilde_prime + n1
       result = ceiling(max(min(n2_prime, n_max), n2))
-      is.even <- function(x){ x %% 2 == 0 }
-      if(is.even(result) == F){result = result + 1}
+
+      if(!is.even(result)){result = result + 1}
     }else{
       if(cp < cp_fu | z1 >= z1_bound){
         result = n1 + overrun
@@ -124,96 +127,104 @@ pz.sim.binary <- function(p1,
     return(result)
   }
   #sample size(re-estimated)
-  results[,5] = apply(results[,1:12],1,function(x)
-    re.est(pz_low,pz_high,n_max,x[1], x[3], x[11],x[12]))
+  results[ ,5] = apply(results[ ,1:12],1,function(x)
+    re.est(pz_low, pz_high, n_max, x[1], x[3], x[11], x[12]))
   #stage 2 sample size(re-estimated)
-  results[,4] = results[,5] - results[,1]
+  results[ ,4] = results[ ,5] - results[ ,1]
   # final test  in stage 2
-  final.test <- function(z1,x1,x2,n2_new,n2,p1,p2,
+  final.test <- function(z1, x1, x2, n2_new, n2, p1, p2,
                          cp, n2_plan = NULL, n_plan = NULL){
     if (is.null(n2_plan)) {
       if (cp < cp_fu & z1 < z1_bound) {
-        result = -Inf #if stop for futiltiy 
+        result = -Inf #if stop for futility 
       }else if(cp >= cp_fu & z1 >= z1_bound){
         result = Inf #if stop for efficacy
       }else{
-        trt = rbinom(1, n2_new/2,p1)
-        ctrl = rbinom(1, n2_new/2,p2)
-        result = testBinomial(x1+trt,x2+ctrl,n2/2,n2/2,
-                              adj=1,scale = scale)
+        trt = rbinom(1, n2_new/2, p1)
+        ctrl = rbinom(1, n2_new/2, p2)
+        result = testBinomial(x1+trt, x2+ctrl, n2/2, n2/2,
+                              adj = adjust, scale = scale)
       } #cont. to stage 2
       
     }else{
-      trt = rbinom(1, n2_plan/2,p1)
-      ctrl = rbinom(1, n2_plan/2,p2)
-      result = testBinomial(x1+trt,x2+ctrl,n_plan/2,n_plan/2,
-                            adj=1,scale = scale)
+      trt = rbinom(1, n2_plan/2, p1)
+      ctrl = rbinom(1, n2_plan/2, p2)
+      result = testBinomial(x1+trt, x2+ctrl, n_plan/2, n_plan/2,
+                            adj = adjust, scale = scale)
     } #no change made to sample size
     return(result) 
   }
   # test statistic in stage 2: z2_adaptive
-  results[,15] = apply(results[,1:13],1,function(x)
+  results[ ,15] = apply(results[,1:13], 1, function(x)
     final.test(x[11], x[6], x[8], x[4], x[5], p1, p2, cp = x[12]))
-  results[,16] = as.numeric(results[,15] >= z2_bound) 
+  results[ ,16] = as.numeric(results[,15] >= z2_bound) 
   
-  wald = rep(0,nsim)
-  wald =  apply(results[,1:13],1,function(x){    
-    final.test(x[11],x[6], x[8], x[4],x[5],p1,p2,
-               cp = x[12],n2_plan = x[2] ,n_plan = x[3])
+  wald = rep(0, nsim)
+  wald =  apply(results[ ,1:13], 1, function(x){    
+    final.test(x[11], x[6], x[8], x[4], x[5], p1, p2,
+               cp = x[12], n2_plan = x[2] ,n_plan = x[3])
   })
   
   results[ ,17] = results[ ,15]
   # GSD stage 2 test statistic
   results[results[ ,12] >= pz_low & results[ ,12] < pz_high, 17] = wald[results[ ,12] >= pz_low & results[ ,12] < pz_high]
   # test statistic in stage 2: z2_sequential 
-  results[ ,18] = as.numeric(results[,17] >= z2_bound)
+  results[ ,18] = as.numeric(results[ ,17] >= z2_bound)
   
-  #temp = results[results[,12] >= pz_low & results[,12] < pz_high,]
-  #library("tidyverse")
-  #temp = as.data.frame(temp)
-  #temp1 = temp %>%
-  #  count(`Total sample size (re-estimated)`,`Stage 1 TRT success`,
-  #        `Stage 1 TRT failure`,`Stage 1 CTRL success`,`Stage 1 CTRL failure`,
-  #        `CP`)
-  #temp2 = temp %>%
-  #  count(`Total sample size (re-estimated)`, `CP`)
-  #temp2$total = temp2$`Total sample size (re-estimated)`*temp2$n
-  #sum(temp2$total)/sum(temp2$n)
   
-  cat("\nProb. futility", sum(results[,12] < cp_fu)/nsim)
-  cat("\nProb. unfavorable", sum(results[,12] < pz_low)/nsim)
-  cat("\nProb. promising", sum(results[,12] >= pz_low & results[,12] < pz_high)/nsim)
-  cat("\nProb. favorable", sum(results[,12] >= pz_high)/nsim)
+  cat("\nProb. futility", sum(results[ ,12] < cp_fu)/nsim)
+  cat("\nProb. unfavorable", sum(results[ ,12] < pz_low)/nsim)
+  cat("\nProb. promising", sum(results[ ,12] >= pz_low & results[ ,12] < pz_high)/nsim)
+  cat("\nProb. favorable", sum(results[ ,12] >= pz_high)/nsim)
+  cat("\nProb. efficacy", sum(results[ ,11] >= z1_bound)/nsim)
   cat("\n")
-  cat("\nAverage sample size (unfavorable)", mean(results[results[,12] < pz_low,5]))
-  cat("\nAverage sample size (promising)", mean(results[results[,12] >= pz_low & results[,12] < pz_high,5]))
-  cat("\nAverage sample size (favorable)", mean(results[results[,12] >= pz_high,5]))
+  cat("\nAverage sample size (unfavorable)", mean(results[results[ ,12] < pz_low, 5]))
+  cat("\nAverage sample size (promising)", mean(results[results[ ,12] >= pz_low & results[ ,12] < pz_high, 5]))
+  cat("\nAverage sample size (favorable)", mean(results[results[ ,12] >= pz_high, 5]))
   cat("\n")
   cat("\n##Sequential Design##")
-  cat("\nPower conditional on interim outcome", sum(results[results[,12] < pz_low, 18])/sum(results[,12] < pz_low))
-  cat("\nPower conditional on interim outcome", sum(results[results[,12] >= pz_low & results[,12] < pz_high, 18])/sum(results[,12] >= pz_low & results[,12] < pz_high))
-  cat("\nPower conditional on interim outcome", sum(results[results[,12] >= pz_high, 18])/sum(results[,12] >= pz_high))
+  cat("\nPower conditional on interim outcome", sum(results[results[ ,12] < pz_low, 18])/sum(results[ ,12] < pz_low))
+  cat("\nPower conditional on interim outcome", sum(results[results[ ,12] >= pz_low & results[ ,12] < pz_high, 18])/sum(results[ ,12] >= pz_low & results[ ,12] < pz_high))
+  cat("\nPower conditional on interim outcome", sum(results[results[ ,12] >= pz_high, 18])/sum(results[ ,12] >= pz_high))
   cat("\nOverall Power ", sum(results[, 18])/nsim)
   cat("\n")
   cat("\n##Adaptive Design##")
-  cat("\nPower conditional on interim outcome", sum(results[results[,12] < pz_low, 16])/sum(results[,12] < pz_low))
-  cat("\nPower conditional on interim outcome", sum(results[results[,12] >= pz_low & results[,12] < pz_high, 16])/sum(results[,12] >= pz_low & results[,12] < pz_high))
-  cat("\nPower conditional on interim outcome", sum(results[results[,12] >= pz_high, 16])/sum(results[,12] >= pz_high))
-  cat("\nOverall Power ", sum(results[, 16])/nsim)
+  cat("\nPower conditional on interim outcome", sum(results[results[ ,12] < pz_low, 16])/sum(results[ ,12] < pz_low))
+  cat("\nPower conditional on interim outcome", sum(results[results[ ,12] >= pz_low & results[ ,12] < pz_high, 16])/sum(results[ ,12] >= pz_low & results[ ,12] < pz_high))
+  cat("\nPower conditional on interim outcome", sum(results[results[ ,12] >= pz_high, 16])/sum(results[ ,12] >= pz_high))
+  cat("\nOverall Power ", sum(results[ ,16])/nsim)
   
   
   # return(results)
 }
 
+pz.sim.binary(p1 = 0.60,
+              p2 = 0.30,
+              alpha = 0.025,
+              n_max = 148,
+              timing = 0.5,
+              pz_low = 0.3556,
+              pz_high = 0.8,
+              cp_fu = 0.15,
+              adjust = 0,
+              nsim = 10000,
+              k = 2,
+              sfu = sfLDOF,
+              sfupar = 1,
+              scale = "diff",
+              seed = 5,
+              overrun = 0,
+              n1 = 38,
+              n2 = 74)
 
 #CP to risk diff
-cp.to.diff <- function(x1=NULL, x2=NULL, n1, n2, cp_fu=0.15){
+cp.to.diff <- function(x1 = NULL, x2 = NULL, n1, n2, adjust = 0, cp_fu=0.15){
   #
-  if(is.null(x1) == F & is.null(x2) == F){
+  if(!is.null(x1) & !is.null(x2)){
     n_trt = n1/2
     n_ctrl = n1/2
     z = testBinomial(x1 = x1, x2 = x2,
-                     n1 = n_trt, n2 = n_ctrl, adj=1, scale = "Difference")
+                     n1 = n_trt, n2 = n_ctrl, adj = adjust, scale = "Difference")
     cp_temp = cp(n1, n2, z)
     cat("\nCP", cp_temp)
   }else{
@@ -227,7 +238,7 @@ cp.to.diff <- function(x1=NULL, x2=NULL, n1, n2, cp_fu=0.15){
     z2_bound = design$upper$bound[2]
     alpha = 2*(1-pnorm(z2_bound))
     #CP
-    cp <- function(n1,n2,z1){
+    cp <- function(n1, n2, z1){
       n2_tilde = n2-n1
       1 - pnorm(((z2_bound*sqrt(n2) - z1*sqrt(n1)) / sqrt(n2_tilde)) -
                   ((z1*sqrt(n2_tilde)) / sqrt(n1)))
@@ -242,12 +253,12 @@ cp.to.diff <- function(x1=NULL, x2=NULL, n1, n2, cp_fu=0.15){
       if(is.null(x1) == F & is.null(x2) == T){
         x2_temp = x2_temp - 1
         z = testBinomial(x1 = x1, x2 = x2_temp,
-                         n1 = n_trt, n2 = n_ctrl, adj=1, scale = "Difference")
+                         n1 = n_trt, n2 = n_ctrl, adj = adjust, scale = "Difference")
         cp_temp = cp(n1, n2, z)
       }else if(is.null(x1) == T & is.null(x2) == F){
         x1_temp = x1_temp + 1
         z = testBinomial(x1 = x1_temp, x2 = x2,
-                         n1 = n_trt, n2 = n_ctrl, adj=1, scale = "Difference")
+                         n1 = n_trt, n2 = n_ctrl, adj = adjust, scale = "Difference")
         cp_temp = cp(n1, n2, z)
       }
     }
@@ -264,5 +275,5 @@ cp.to.diff <- function(x1=NULL, x2=NULL, n1, n2, cp_fu=0.15){
     }
   }
 }
-cp.to.diff(x1=11, x2=NULL, n1=38, n2=74, cp_fu=0.15)
+cp.to.diff(x1=11, x2=NULL, n1=38, n2=74, adjust = 0, cp_fu=0.15)
 
